c9881994b2104095bcfbad1faf8d9fbfc02ea88e
bread34567 <bread34567@gmail.com>
2023-02-01 20:33:13 +0800

    ISA all instruction

--- FiveStage_RISB/obj_dir/testbench.d
+++
@@ -1,9 +0,0 @@
-testbench.o: ../testbench.cpp Vcoretop.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/vltstd/svdpi.h Vcoretop__Syms.h \
- Vcoretop.h Vcoretop___024root.h Vcoretop_coretop.h Vcoretop_dpram.h \
- Vcoretop_regfiles.h /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated_trace.h \
- /usr/local/share/verilator/include/verilated_trace_defs.h
