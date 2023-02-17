7cd0344bfdac4ae85299c2dd298a7244a89d9b62
bread34567 <bread34567@gmail.com>
2023-02-17 21:39:41 +0800

    add bus console unit & ctest program

---
+++ Soc_rv32im/testbench/tb/sim_mem.cpp
@@ -0,0 +1,65 @@
+#include <cstdint>
+#include <cstdlib>
+#include "verilated_vcd_c.h"
+#include "Vsoc_top.h"
+#include "Vsoc_top__Syms.h"
+#include "verilated.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+
+
+#define bswap from_le
+
+//X86 machine
+template<typename T> static inline T from_le(T n) { return n; }
+
+void sim_mem_write(Vsoc_top_dpram__R200000_RB15* dpram,uint32_t addr, size_t length, const void* bytes)
+{
+    //out of boundary protection?
+    for (int i = 0 ; i < length ; i +=4 ) {
+        dpram->writeByte(addr+i,*((unsigned char*)bytes+i+3));
+        dpram->writeByte(addr+i+1,*((unsigned char*)bytes+i+2));
+        dpram->writeByte(addr+i+2,*((unsigned char*)bytes+i+1));
+        dpram->writeByte(addr+i+3,*((unsigned char*)bytes+i));
+    }
+}
+
+int sim_mem_read(Vsoc_top_dpram__R200000_RB15* dpram,uint32_t addr, size_t length)
+{
+    //out of boundary protection?
+    //printf("%p\n",addr);
+    int x;
+    for (int i = 0 ; i < length ; i +=4 ) {
+        uint32_t c0,c1,c2,c3;
+        dpram->readByte(addr+i,c0);
+        dpram->readByte(addr+i+1,c1);
+        dpram->readByte(addr+i+2,c2);
+        dpram->readByte(addr+i+3,c3);
+        x = c0<<24|c1<<16|c2<<8|c3;
+    }
+    return x;
+}
+
+uint32_t sim_regs_read(Vsoc_top_regfiles* regs,uint32_t addr)
+{
+    uint32_t v;
+    regs->readRegister(addr, v);
+    //printf("read register\n");
+    return v;
+}
+void sim_mem_load_bin(Vsoc_top_dpram__R200000_RB15* dpram, std::string fn)
+{
+    std::ifstream bpfs(fn, std::ios::binary|std::ios::ate);
+    std::ifstream::pos_type pos = bpfs.tellg();
+    int f_length = pos;
+    char * buf = new char[f_length];
+    bpfs.seekg(0, std::ios::beg);
+    bpfs.read(buf, f_length);
+    bpfs.close();
+    //printf("file size: %d\n", f_length);
+
+    for(int i=0; i<f_length;i+=4) {
+        sim_mem_write(dpram, bswap(i),4,(uint8_t*)buf+i);
+    }
+}
